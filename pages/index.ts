c93bfc4b4152ab99f31dc90dc9d5f35ce6702eac
Sleezzi <ilan.dfp91@gmail.com>
2024-03-28 12:14:09 +0100

    .

--- pages/index.ts
+++ pages/index.ts
@@ -1,7 +1,7 @@
 import express, { Express, Request, Response } from "express";
 module.exports = {
-    name: "/new_ticket",
-    type: "POST",
+    name: "/",
+    type: "GET",
     execute(request: Request, response: Response) {
         response.status(200).json({status: 200});
     }
